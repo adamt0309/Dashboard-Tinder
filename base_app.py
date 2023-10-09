a963112a95b8744d9a020ff6b1dc9c0b8e6aca74
Adam Tran <adamt0309@gmail.com>
2023-10-09 19:54:00 +0100

    test

--- base_app.py
+++ base_app.py
@@ -280,7 +280,7 @@ def update_charts(match_value,swipe_value):
 #############################################################################################################################
 
 if __name__ == '__main__':
-    dash_app.run_server("0.0.0.0", 8000, debug = True)
+    dash_app.run_server(debug = True)
 
  # app = dash_app.server
 
